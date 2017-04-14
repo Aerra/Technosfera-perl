fefa3104aad657b38a276ff49c7e4117eeb65b1a
anna <6v-1534krasilnik@mail.ru>
2017-04-14 17:13:45 +0300

    question4

--- homeworks/myconst/lib/myconst.pm
+++ homeworks/myconst/lib/myconst.pm
@@ -3,7 +3,9 @@ package myconst;
 use strict;
 use warnings;
 use Scalar::Util 'looks_like_number';
-
+use Exporter qw(import); 
+our %declared_constants;
+use Data::Dumper;
 
 =encoding utf8
 
@@ -37,6 +39,68 @@ print ZERO;             # 0
 print PI;               # 3.14
 =cut
 
+my $normal_constant_name = qr/^_?[^\W_0-9]\w*\z/;
+my $tolerable = qr/^[A-Za-z_]\w*\z/;
 
+#sub import
+our @EXPORT_OK;
+our %EXPORT_TAGS;
+my $class = shift;    #здесь должен быть math
+#	return unless @_;
+	#printf "$class\n";
+my $caller = caller; 
+my $multiple  = ref $_[0]; 
+my $constants;
+my $group;
+my $pkg; #what?!
+# my $symtab = \%{$caller.':'};
 
+	if ($multiple)
+	{
+		if (ref($_) ne 'HASH') 
+		{
+			die "Invalide type for constant\n";
+		}
+		$group=shift;  #here ref to hash
+		#printf ("$group\n");
+		my $name;
+		foreach $name (keys %$group)   #$name is name of constant
+		{
+			#if ref(%$constants->{$name})
+			#{
+			#	die "It's not a constant!\n";
+			#}
+			if ($name !=~ $normal_constant_name or $name !=~ $tolerable)
+			{
+				die "It's not normal constant name!\n";
+			}
+			if (looks_like_number($group->{$name}))
+			{
+				push @EXPORT_OK, $name;
+				$EXPORT_TAGS{all}{$name}=$group->{$name};
+				$EXPORT_TAGS{$class}{$name}=$group->{$name};	
+			}
+			else 
+			{
+				die "It's not a number!\n";
+			}
+		}
+	}
+	else
+	{
+		if (defined $class)
+		{
+			my $name=shift;
+			#die "Can't use undef as constant name\n";
+			if ($class !=~ $normal_constant_name or $class !=~ $tolerable)
+			{
+				die "It's not normal constant name!\n";
+			}
+			if (looks_like_number($name))
+			{
+				$EXPORT_TAGS{all}{$class}=$name;
+				$EXPORT_TAGS{$class}=$name;	
+			}
+		}	
+	}
 1;
