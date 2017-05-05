5f1492fa8c0ca57bbbd5dea29ab90ec2eeae5ce7
Aerra <6v-1534krasilnik@mail.ru>
2017-05-05 23:46:32 +0300

    now it work!

--- homeworks/myconst/lib/myconst.pm
+++ homeworks/myconst/lib/myconst.pm
@@ -41,66 +41,115 @@ print PI;               # 3.14
 
 my $normal_constant_name = qr/^_?[^\W_0-9]\w*\z/;
 my $tolerable = qr/^[A-Za-z_]\w*\z/;
+our %const_hash;
+our %group_hash;
 
-#sub import
-our @EXPORT_OK;
-our %EXPORT_TAGS;
-my $class = shift;    #здесь должен быть math
-#	return unless @_;
-	#printf "$class\n";
-my $caller = caller; 
-my $multiple  = ref $_[0]; 
-my $constants;
-my $group;
-my $pkg; #what?!
-# my $symtab = \%{$caller.':'};
 
-	if ($multiple)
+sub import{
+	our @EXPORT_OK;
+	our %EXPORT_TAGS;
+	my ($class, @args) = @_;    #здесь должен быть math
+	#return unless @_;
+	printf "$class\n";
+	die "invalid args checked" if scalar(@args)==1;
+	foreach (@args)
 	{
-		if (ref($_) ne 'HASH') 
+		die "Bad arguments!\n" unless (defined $_);
+	}
+	my %args=(@args);
+	my $caller = caller; 
+	#my $multiple  = ref $_[0]; 
+	my $constant;
+	my $group;
+	#my $pkg; #what?!
+	# my $symtab = \%{$caller.':'}
+	#if ($multiple)
+	#{
+	for my $key (keys %args)
+	{
+		die "empty const\n" if $key eq '';
+		if (ref($args{$key}))
 		{
-			die "Invalide type for constant\n";
-		}
-		$group=shift;  #here ref to hash
-		#printf ("$group\n");
-		my $name;
-		foreach $name (keys %$group)   #$name is name of constant
-		{
-			#if ref(%$constants->{$name})
-			#{
-			#	die "It's not a constant!\n";
-			#}
-			if ($name !=~ $normal_constant_name or $name !=~ $tolerable)
+			if (ref($args{$key}) ne 'HASH') 
 			{
-				die "It's not normal constant name!\n";
+				die "Invalide type for constant\n";
 			}
-			if (looks_like_number($group->{$name}))
+			die "empty const\n" if $args{$key} eq '';
+			$group=$key;  #here ref to hash
+			#printf ("$group\n");
+			my $name;
+			foreach $constant (keys %{$args{$key}})   #$name is name of constant
 			{
-				push @EXPORT_OK, $name;
-				$EXPORT_TAGS{all}{$name}=$group->{$name};
-				$EXPORT_TAGS{$class}{$name}=$group->{$name};	
+				#if ($constant !=~ $normal_constant_name or $constant !=~ $tolerable)
+				#{
+				#	die "It's not normal constant name!\n";
+				#}
+				#if (looks_like_number($args{$key}{$constant}))
+				#{ 
+				die "empty const\n" if $constant eq '';
+				die "empty const\n" if $args{$key}{$constant} eq '';
+				die "empty const\n" if ref $args{$key}{$constant};
+				$const_hash{$caller}{$constant}=$args{$key}{$constant};
+				$group_hash{all}{$constant}=$args{$key}{$constant};
+				$group_hash{$group}{$constant}=$args{$key}{$constant};
+				$EXPORT_TAGS{all}{$constant}=$args{$key}{$constant};
+				$EXPORT_TAGS{$group}{$constant}=$args{$key}{$constant};	
+				#}
+				#else 
+				#{
+				#	die "It's not a number!  $args{$key}{$constant} \n";
+				#}
+			}
+		}
+		else
+		{
+			die "empty const" if $args{$key} eq '';
+			$constant=$key;
+			#if ($constant !=~ $normal_constant_name or $constant !=~ $tolerable)
+			#{
+			#	die "It's not normal constant name!\n";
+			#}
+			#if (looks_like_number($args{$key}))
+			#{
+			$EXPORT_TAGS{all}{$constant}=$args{$key};
+			$const_hash{$caller}{$constant}=$args{$key};
+			$group_hash{all}{$constant}=$args{$key};
+			#}
+			#else 
+			#{
+			#	die "It's not a number!  $args{$key}  $key \n";
+			#}
+		}	
+	}	
+	#}
+	no strict 'refs';
+	for $constant (keys %{$const_hash{$caller}})
+	{
+		*{"$caller::$constant"}=sub () {$const_hash{$caller}{$constant};};	
+	}
+
+	#*{"$caller"."::import"} = \&Exporter::import;
+	my $call=$caller;
+
+	*{"$caller"."::import"} = sub {
+		my $pkg = shift;
+		my @required= @_;
+		my $caller=caller;
+		foreach my $r (@required)
+		{
+			if ($r =~ /^:(.+)$/) 
+			{
+				for my $key (keys $group_hash{$1})
+				{
+					my $pf=$1;
+					*{"$caller::$key"} = sub () {$group_hash{$pf}{$key};}
+				}
 			}
 			else 
 			{
-				die "It's not a number!\n";
+				*{"$caller::$r"} = sub () { $const_hash{$call}{$r};} 
 			}
 		}
 	}
-	else
-	{
-		if (defined $class)
-		{
-			my $name=shift;
-			#die "Can't use undef as constant name\n";
-			if ($class !=~ $normal_constant_name or $class !=~ $tolerable)
-			{
-				die "It's not normal constant name!\n";
-			}
-			if (looks_like_number($name))
-			{
-				$EXPORT_TAGS{all}{$class}=$name;
-				$EXPORT_TAGS{$class}=$name;	
-			}
-		}	
-	}
+}
 1;
