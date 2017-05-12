0b10d64c368da93b5a85cd0f7c75204d4f910fa0
Aerra <6v-1534krasilnik@mail.ru>
2017-05-12 15:14:37 +0300

    good works

--- homeworks/pr_server/tcp_server.pl
+++ homeworks/pr_server/tcp_server.pl
@@ -7,54 +7,55 @@ use AnyEvent::HTTP;
 use AnyEvent::Socket;
 use AnyEvent::Handle;
 use DDP;
-use feature qw(say);
 
 my $cv=AE::cv;
 
 $SIG{INT}=sub {
+	#printf "GoodBye!\n";
 	$cv->send();
 };
 
 my %connect;
 
-printf "Good!\n";
+#printf "Hello!\n";
 
 tcp_server '127.0.0.1', 1234, sub {
-	printf "Good1\n";
-	my ($fh, $host, $port)=@_ or $cv->send($!);
+	my ($fh, $host, $port)=@_;
+	$cv->send($!) unless defined $fh;
 	my $peer = join(':', $host,$port);
-	printf "$peer\n";
+	#printf "Your client connected to: $peer\n";
 	my $h = AnyEvent::Handle->new(
 		fh => $fh,
 	);
 	$h->on_error (sub {
 		my ($handle, $fatal, $message)=@_;
+		#printf "I have bad news: $message \n";
 		$h->destroy;
 		delete $connect{$handle};
 		#$cv->send();
 	});
 	$h->on_eof (sub {
 		my $handle=shift;
+		#printf "Bye!\n";
 		$h->destroy;
 		delete $connect{$handle};
 	});
 
 	$h->on_read (sub {
 		my $handle=shift;
+		#printf "Uhhu! I like read! ($handle)\n";
 		$handle -> push_read ( line => sub {
-			printf "In read\n";
 			my ($handle, $line)=@_;
-			printf "$line\n";
+			#printf "Reading $line\n";
 			if ($line =~ /^URL\s(.*)$/)
 			{
-				printf "In URL\n";
+				#printf "You give me URL: $1\n";
 				$handle->push_write("OK\n");
 				$connect{$peer}=$1;
-				printf "$connect{$peer}";
 			}
 			elsif ($line eq 'HEAD')
 			{
-				printf "In HEAD\n";
+				#printf "You ask me give you the HEAD\n";
 				if ($connect{$peer})
 				{
 					http_head
@@ -65,23 +66,28 @@ tcp_server '127.0.0.1', 1234, sub {
 						my $length=0;
 						foreach my $key (keys %{$headers})
 						{
+							my $head = $key.': '.$headers->{$key}."\n";
 							push @array, $key.': '.$headers->{$key}."\n";
-							$length += $key.': '.$headers->{$key}."\n";
+							$length += length $head;
 						}
 						$handle->push_write("OK $length\n");
+						#printf "I say, that your HEAD: OK $length\n";
 						foreach (@array)
 						{
 							$handle->push_write($_);
+							#printf "$_\n";
 						}
 					};
 				}
 				else
 				{
+					#printf "HEAD: You don't give me URL before:(\n";
 					$handle->push_write("where URL?\n");
 				}
 			}
 			elsif ($line eq 'GET')
 			{
+				#printf "You ask me give you the GET\n";
 				if ($connect{$peer})
 				{
 					http_get
@@ -90,22 +96,28 @@ tcp_server '127.0.0.1', 1234, sub {
 						my $data = shift;
 						my $length = length $data;
 						$handle->push_write("OK $length\n");
+						#printf "I say, that your GET: OK $length\n";
 						$handle->push_write($data);
+						#printf "$data\n";
 					};	
 				}
 				else
 				{
+					#printf "GET: You don't give me URL before:(\n";
 					$handle->push_write("where URL?\n");
 				}
 			}
 			elsif ($line eq 'FIN')
 			{
+				#printf "You ask me give you the FIN\n";
 				$handle->push_write("OK\n");
 				delete $connect{$handle};
 				$handle->destroy;
+				#printf "FIN: OK\n";
 			}
 			else
 			{
+				#printf "You ask for garbage\n";
 				$handle->push_write("it's not a normal command!\n");
 			}
 		});
